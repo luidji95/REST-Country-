253237f5f44d23478ccdad3acd0bcf199e8df13e
Milos Petrovic <milos.petrovic09.95@gmail.com>
2024-09-26 22:16:30 +0200

    light and dark mode functionality

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
 import './style.css'
+
+const lightModeSwitch = document.querySelector('.light-mode');
+const darkModeSwitch = document.querySelector('.dark-mode');
+const lightImg = document.getElementById('light-img');
+const darkImg = document.getElementById('dark-img');
+const darkLogo = document.querySelector('.logo-darkmode');
+const lightLogo = document.querySelector('.logo-light-mode');
+const header = document.querySelector('.body');
+const h1 = document.querySelector('.country-app');
+const countryList = document.getElementById('country-list');
+
+// Klik na light mode ikonicu
+lightImg.addEventListener('click', function() {
+    lightModeSwitch.classList.add('hidden');   
+    darkModeSwitch.classList.remove('hidden'); 
+    darkModeSwitch.classList.add('visible');   
+    darkLogo.classList.remove('hidden');
+    lightModeSwitch.classList.add('hidden');
+    header.classList.add('color');
+    h1.classList.add('countryappcolor');
+
+});
+
+// Klik na dark mode ikonicu
+darkImg.addEventListener('click', function() {
+    darkModeSwitch.classList.add('hidden');    
+    darkModeSwitch.classList.remove('visible'); 
+    lightModeSwitch.classList.remove('hidden'); 
+    darkLogo.classList.add('hidden');
+    lightLogo.classList.remove('hidden');
+    header.classList.remove('color');
+    h1.classList.remove('countryappcolor');
+});
